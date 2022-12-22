936159896964ab07205c47df5d97631495a52248
Silvio Custodio <adm.ti.silviocustodio@gmail.com>
2022-12-22 11:46:55 +0000

    feat: Added  react-router-dom

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,1 +1,13 @@
-export {};
+// import { Button } from '@mui/material';
+import { Routes, Route, Navigate } from 'react-router-dom';
+// import { useDrawerContext } from '../shared/contexts';
+
+export const AppRoutes = () => {
+  //   const { toggleDrawerOpen } = useDrawerContext();
+  return (
+    <Routes>
+      <Route path="/start-page" element={<p>Start page</p>} />
+      <Route path="*" element={<Navigate to="/start-page" />} />
+    </Routes>
+  );
+};
